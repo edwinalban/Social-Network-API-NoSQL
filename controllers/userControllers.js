1572a0d5da5c6af5a78c4cd63db8cf63fe0b184f
Yachtzee <edwinalban@gmail.com>
2023-09-02 20:59:12 -0700

    udpated userControllers.js to add/delete object id to/from friends array

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -39,7 +39,7 @@ module.exports = {
 
     async updateUser(req, res) {
         try {
-            let updatedUser = await User.findOneAndUpdate({ _id: req.params.userId }, {$set: req.body}, {returnOriginal: false, runValidators: true});
+            let updatedUser = await User.findOneAndUpdate({ _id: req.params.userId }, { $set: req.body }, { returnOriginal: false, runValidators: true });
 
             if (!updatedUser) {
                 return res.status(404).json({ message: 'No user with that ID' })
@@ -71,15 +71,13 @@ module.exports = {
     async addFriend(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId });
-
             if (!user) {
                 return req.status(404).json({ message: 'No user with that ID' });
             }
-
-            let friends = this.friends;
-            const newFriends = friends.push(req.params.friendId);
-            friends = newFriends;
-            res.json(friends, { message: 'Friend added!' });
+            let friend = req.params.friendId;
+            user.friends.push(friend)
+            user.save();
+            res.json({ message: 'Friend added!' });
 
         } catch (e) {
             res.status(500).json(e);
@@ -94,10 +92,10 @@ module.exports = {
                 return req.status(404).json({ message: 'No user with that ID' });
             }
 
-            let friends = this.friends;
-            const newFriends = friends.splice(req.params.friendId);
-            friends = newFriends;
-            res.json(friends, { message: 'Friend deleted!' });
+            let friend = req.params.friendId;
+            user.friends.splice(friend);
+            user.save();
+            res.json({ message: 'Friend deleted!' });
 
         } catch (e) {
             res.status(500).json(e);

7e403a2ede1f730637946bcb1459793b44f57216
Yachtzee <edwinalban@gmail.com>
2023-09-02 22:07:47 -0700

    updated deleteFriend function in userControllers.js to remove only the id that matches the request parameters, was deleting all friends ids previously

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -71,9 +71,11 @@ module.exports = {
     async addFriend(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId });
+
             if (!user) {
                 return req.status(404).json({ message: 'No user with that ID' });
             }
+
             let friend = req.params.friendId;
             user.friends.push(friend)
             user.save();
@@ -93,7 +95,7 @@ module.exports = {
             }
 
             let friend = req.params.friendId;
-            user.friends.splice(friend);
+            user.friends.remove(friend);
             user.save();
             res.json({ message: 'Friend deleted!' });
 
